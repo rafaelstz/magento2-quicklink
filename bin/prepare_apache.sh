e7057059f00239940701b4fc3b62ff0872498a98
Rafael Correa Gomes <rafaelcg_stz@hotmail.com>
2021-01-05 12:48:27 -0500

    Travis CI

--- bin/prepare_apache.sh
+++ bin/prepare_apache.sh
@@ -13,4 +13,4 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php
 sudo sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
 sudo chown -R travis:travis /var/lib/apache2/fastcgi
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-sudo cp -f Resources/travis/apache.conf /etc/apache2/sites-available/000-default.conf
+sudo cp -f bin/apache.conf /etc/apache2/sites-available/000-default.conf
